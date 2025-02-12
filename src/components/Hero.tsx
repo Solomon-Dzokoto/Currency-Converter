99c13764e9add3e97bbe5e19cba676c5b535a15c
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-02-12 08:09:01 +0000

    change the color appearance

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
     return (
 
-        <header className="px-[clamp(1rem,4vw,4rem)] min-h-screen bg-[url(../assets/background.jpg)] bg-cover relative after:absolute after:inset-0 after:bg-violet-950 after:opacity-95  after:content-[''] py-6">
+        <header className="px-[clamp(1rem,4vw,4rem)] min-h-screen bg-[url(../assets/background.jpg)] bg-cover relative after:absolute after:inset-0 after:bg-blue-950 after:opacity-95  after:content-[''] py-6">
             <NavBar />
             <section className=" md:mt-[5rem] mt-[2rem] min-h-[90vh] ">
                 <div className="grid  md:grid-cols-2 relative min-h-[70vh] z-10 h-full text-white">
